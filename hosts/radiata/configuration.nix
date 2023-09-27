e5eda1bbd09f1bf74666b53d38df588d7526167a
gyaru <gyaru@users.noreply.github.com>
2023-09-27 06:38:08 +0200

    change rollback to systemd service add persisted files service for files needed extra early add passwd/shadow to persist because we're not rolling passwords atm

--- hosts/radiata/configuration.nix
+++ hosts/radiata/configuration.nix
@@ -93,21 +93,49 @@
     initrd = {
       systemd.enable = true;
       supportedFilesystems = ["btrfs"];
-      postDeviceCommands = pkgs.lib.mkBefore ''
-        mkdir -p /mnt
-        mount -o subvol=/ UUID=caf259ee-b2be-4cf8-b41a-752a09d344a7 /mnt
-        btrfs subvolume list -o /mnt/root |
-        cut -f9 -d' ' |
-        while read subvolume; do
-          echo "deleting /$subvolume subvolume..."
-          btrfs subvolume delete "/mnt/$subvolume"
-        done &&
-        echo "deleting /root subvolume..." &&
-        btrfs subvolume delete /mnt/root
-        echo "restoring blank /root subvolume..."
-        btrfs subvolume snapshot /mnt/root-blank /mnt/root
-        umount /mnt
-      '';
+      systemd.services.rollback = {
+        description = "rollback BTRFS root subvolume to a pristine state";
+        wantedBy = [
+          "initrd.target"
+        ];
+        before = [
+          "sysroot.mount"
+        ];
+        unitConfig.DefaultDependencies = "no";
+        serviceConfig.Type = "oneshot";
+        script = ''
+          mkdir -p /mnt
+          mount -t btrfs -o subvol=/ /dev/nvme0n1p4 /mnt
+          btrfs subvolume list -o /mnt/root |
+            cut -f9 -d' ' |
+            while read subvolume; do
+              echo "deleting /$subvolume subvolume..."
+              btrfs subvolume delete "/mnt/$subvolume"
+            done &&
+            echo "deleting /root subvolume..." &&
+            btrfs subvolume delete /mnt/root
+          echo "restoring blank /root subvolume..."
+          btrfs subvolume snapshot /mnt/root-blank /mnt/root
+          umount /mnt
+        '';
+      };
+      systemd.services.persisted-files = {
+        description = "hard-link persisted files from /persist";
+        wantedBy = [
+          "initrd.target"
+        ];
+        after = [
+          "sysroot.mount"
+        ];
+        unitConfig.DefaultDependencies = "no";
+        serviceConfig.Type = "oneshot";
+        script = ''
+          mkdir -p /sysroot/etc/
+          ln -snfT /persist/etc/machine-id /sysroot/etc/machine-id
+        '';
+        # ln -snfT /persist/etc/passwd /sysroot/etc/passwd
+        # ln -snfT /persist/etc/shadow /sysroot/etc/shadow
+      };
     };
 
     lanzaboote = {
@@ -185,7 +213,8 @@
         "/var/lib/systemd/coredump"
       ];
       files = [
-        "/etc/machine-id"
+        "/etc/passwd"
+        "/etc/shadow"
       ];
     };
   };
@@ -270,7 +299,7 @@
     };
   };
 
-  # user lis
+  # users
   users.users.lis = {
     isNormalUser = true;
     home = "/home/lis";
