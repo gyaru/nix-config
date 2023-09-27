e0ae04e6eb12cd9e80ae094dd12e3dd07e699935
gyaru <gyaru@users.noreply.github.com>
2023-09-27 05:16:13 +0200

    add postDeviceCommands to replace / with snapshot

--- hosts/radiata/configuration.nix
+++ hosts/radiata/configuration.nix
@@ -93,6 +93,21 @@
     initrd = {
       systemd.enable = true;
       supportedFilesystems = ["btrfs"];
+      postDeviceCommands = pkgs.lib.mkBefore ''
+        mkdir -p /mnt
+        mount -o subvol=/ UUID=caf259ee-b2be-4cf8-b41a-752a09d344a7 /mnt
+        btrfs subvolume list -o /mnt/root |
+        cut -f9 -d' ' |
+        while read subvolume; do
+          echo "deleting /$subvolume subvolume..."
+          btrfs subvolume delete "/mnt/$subvolume"
+        done &&
+        echo "deleting /root subvolume..." &&
+        btrfs subvolume delete /mnt/root
+        echo "restoring blank /root subvolume..."
+        btrfs subvolume snapshot /mnt/root-blank /mnt/root
+        umount /mnt
+      '';
     };
 
     lanzaboote = {
